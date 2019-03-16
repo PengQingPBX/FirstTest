30a57ccb05d56feda8098777ac8d95548ae05f33
PengQing <13527319590@163.com>
2019-03-16 08:54:07 +0800

    添加项目文件。

---
+++ Json/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Json.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult JsonTest()
+        {
+            return View();
+        }
+        public ActionResult JsonTest(int a=0)
+        {
+            return Json(new { name="脾气"});
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TE/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write( string.IsNullOrWhiteSpace("value"));
+            Console.Write( string.IsNullOrWhiteSpace(""));
+            Console.Write( string.IsNullOrWhiteSpace("  "));
+            //query_Rtn + string.Empty
+            string query_Rtn = null;
+            Console.Write((query_Rtn+"").Length+"\n\r");
+            string url = string.Format("ui", 3, 2);
+            Console.Write(url);
+        }
+    }
+}
